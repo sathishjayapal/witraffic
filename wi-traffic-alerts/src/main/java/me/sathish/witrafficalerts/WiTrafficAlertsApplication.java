d7902bac6c47f3bdd5c318e7cab81ccdf0d5cbbd
Sathish Jayapal <sjayapal@skminfotech.com>
2022-04-06 22:03:43 -0500

    Traffic Alert changes

---
+++ wi-traffic-alerts/src/main/java/me/sathish/witrafficalerts/WiTrafficAlertsApplication.java
@@ -0,0 +1,12 @@
+package me.sathish.witrafficalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiTrafficAlertsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WiTrafficAlertsApplication.class, args);
+	}
+
+}
---
+++ wi-traffic-alerts/src/test/java/me/sathish/witrafficalerts/WiTrafficAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sathish.witrafficalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiTrafficAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
